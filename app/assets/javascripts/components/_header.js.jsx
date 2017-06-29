55d5d744f48891aea89096b7faf65c2f014ea6fb
Carlos <carlosprovisorio@icloud.com>
2017-06-29 18:17:11 -0400

    import jquery, create components

---
+++ app/assets/javascripts/components/_header.js.jsx
@@ -0,0 +1,9 @@
+var Header = React.createClass({
+  render() {
+    return (
+    <div>
+      <h1>Hello, Carlos (header component)</h1>
+    </div>
+    )
+  }
+});
---
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -0,0 +1,9 @@
+var NewItem = React.createClass({
+  render() { 
+    return ( 
+			<div>
+				<h1>New item</h1>
+			</div>
+		) 
+	} 
+});
